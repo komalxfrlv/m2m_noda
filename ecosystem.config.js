6ad4754760cf97f9d2f54e6b9aceb66e2fe9bff7
PINGWINbl4 <mikmez01@gmail.com>
2023-07-28 11:33:20 +0500

    pm2 config add

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    apps: [
+      {
+        name: "m2m-node-monolith",
+        script: "./app.js",
+        instances: 4,
+        max_memory_restart: "900M",
+  
+        // Logging
+        out_file: "./out.log",
+        error_file: "./error.log",
+        merge_logs: true,
+        log_date_format: "DD-MM HH:mm:ss Z",
+        log_type: "json",
+  
+        // Env Specific Config
+        env_production: {
+          NODE_ENV: "production",
+          PORT: 5000,
+          exec_mode: "cluster_mode",
+        },
+        env_development: {
+          NODE_ENV: "development",
+          PORT: 5000,
+          watch: true,
+          watch_delay: 10000,
+          ignore_watch: [
+            "./node_modules",
+            "./app/views",
+            "./public",
+            "./.DS_Store",
+            "./package.json",
+            "./yarn.lock",
+            "./samples",
+            "./src"
+          ],
+        },
+      },
+    ],
+  };
+  
