5cd0d0d802d08d7138feff611cf960f1c9c62125
alx <kom.alx.frlv@gmail.com>
2023-08-01 10:59:52 +0500

    zigbee add station

--- src/api/_ecosystem/stations/stations.routes.js
+++ src/api/_ecosystem/stations/stations.routes.js
@@ -5,6 +5,7 @@ router.post('/', station.createNewStation);
 router.get('/', station.getStationById);
 router.put('/settings', station.editSettings);
 router.delete('/', station.deleteStation);
-router.get('/all', station.getAllStations)
+router.get('/all', station.getAllStations);
+router.post('/gateway', station.createGateway)
 
 module.exports = router
