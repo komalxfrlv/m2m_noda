7289f65fb1258607e3c799a129f06d9be81875cb
PINGWINbl4 <mikmez01@gmail.com>
2023-08-15 14:08:15 +0500

    cors XUYH9

--- app.js
+++ app.js
@@ -11,6 +11,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(fileUpload())
 
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+
 BigInt.prototype.toJSON = function() {       
   return this.toString()
 }
