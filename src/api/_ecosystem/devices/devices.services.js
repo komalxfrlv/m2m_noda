59d215613a6a9d38c7226d4015aa7c00a58419af
PINGWINbl4 <mikmez01@gmail.com>
2023-09-20 11:58:47 +0500

    include def scripts at get all deviceTypes

--- src/api/_ecosystem/devices/devices.services.js
+++ src/api/_ecosystem/devices/devices.services.js
@@ -2,7 +2,11 @@ const bcrypt = require('bcrypt');
 const { db } = require('../../../utils/db');
 
 async function getAll() {
-    return await db.DeviceType.findMany();
+    return await db.DeviceType.findMany({
+        include:{
+            defaultScripts: true
+        }
+    });
 }
 
 async function createDevice(device) {
