ccf1d5f96a0427198c6eb0540884000541ded3b2
PINGWINbl4 <mikmez01@gmail.com>
2023-10-18 15:43:31 +0500

    parasha

--- src/api/shelldues/standarts/standart.shelldues.services.js
+++ src/api/shelldues/standarts/standart.shelldues.services.js
@@ -1,11 +1,30 @@
 const { db } = require('../../../utils/db');
 
 async function findAllStandartsByCode(deviceId){
-    return await db.DefaultScripts.findMany({
+    const deviceTypesAtDefaultScripts = await db.deviceTypesAtDefaultScripts.findMany({
         where:{
             deviceId:deviceId
         }
     })
+    return await db.defaultScripts.findMany({
+        where:{
+            id: deviceTypesAtDefaultScripts.defaultScriptId
+        }
+    }) 
+}
+
+async function findAditionalStandartsByCode(deviceId){
+    const deviceTypesAtDefaultScripts = await db.deviceTypesAtDefaultScripts.findMany({
+        where:{
+            deviceId:deviceId
+        }
+    })
+    return await db.defaultScripts.findMany({
+        where:{
+            id: deviceTypesAtDefaultScripts.defaultScriptId,
+            deviceId:{count: 1}
+        }
+    }) 
 }
 
 async function createNewStandartScript(deviceId, script, description){
