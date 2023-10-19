9b5125aa5d916cf755163fbeb9e347fc1c329076
PINGWINbl4 <mikmez01@gmail.com>
2023-10-19 15:55:26 +0500

    better console logs

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  req.payload? console.log(`IP: ${req.ip}\nUserId: ${req.payload.userId}`):console.log(req.payload.logMessage)
   res.status(err.status || 500);
   res.send({
     status: err.status || 500,
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -12,13 +12,16 @@ function isAuthenticated(req, res, next) {
     const token = authorization.split(' ')[1];
 
     const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-
+    
     req.payload = payload;
+    req.payload.logMessage = `\nUserId: ${payload.userId}\nIP:${req.ip}`
+    console.log(req.payload.logMessage)
   } catch (err) {
 
     res.status(401);
 
     console.log(err);
+    console.log(`IP:${req.ip}`)
 
     res.json({ error: err.name });
 
